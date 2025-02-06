0b22db488d9503eba3df6ea00876b2ff6232e9ea
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-06 18:33:10 +0100

    Install shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Logo from '@/components/logo'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
 	return (
 		<main>
-			<Logo />
+			<section>
+				<Logo />
+				<h1>Welcome to Note</h1>
+				<p>Please log in to continue</p>
+
+				<Button>hello</Button>
+			</section>
 		</main>
 	)
 }
