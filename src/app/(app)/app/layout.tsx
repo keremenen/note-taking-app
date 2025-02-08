e2a8841b57aa76c1eddc08772ac2d662adeff3b1
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-08 18:31:43 +0100

    Add header component

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -1,13 +1,27 @@
 import { AppSidebar } from '@/components/app-sidebar'
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { Input } from '@/components/ui/input'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { Settings } from 'lucide-react'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			<SidebarProvider>
 				<AppSidebar />
-				<SidebarTrigger />
-				<main className="w-full min-h-screen">{children}</main>
+				{/* <SidebarTrigger /> */}
+				<main className="w-full min-h-screen">
+					<header className=" px-8 py-4 border-b border-[#E0E4EA] flex items-center">
+						<h1 className="font-bold text-2xl ">All notes</h1>
+						<div className="ml-auto flex items-center gap-4">
+							<Input
+								placeholder="Search by title, content, or tags…"
+								className="w-80"
+							/>
+							<Settings className="ml-auto" />
+						</div>
+					</header>
+					{children}
+				</main>
 			</SidebarProvider>
 		</>
 	)
