5970d61a15f5e9339f428bfd93db4fa51815f53c
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-11 17:10:07 +0100

    WIP data section

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
-import { Settings } from 'lucide-react'
+import { Clock, Settings, Tag } from 'lucide-react'
 
 const notes = [
 	{
@@ -118,7 +118,26 @@ export default function DashboardPage() {
 				</section>
 
 				{/* NOTE DETAILS */}
-				<section className="flex-1">data here</section>
+				<section className="flex-1 px-6 py-5">
+					<h2 className="text-2xl font-bold">React Performance Optimization</h2>
+					<section className="text-sm space-y-1 mt-4">
+						<div className="flex items-center">
+							<span className="flex min-w-28 h-7 items-center gap-x-2">
+								<Tag size={16} />
+								Tags
+							</span>
+							<span>Dev, React</span>
+						</div>
+						<div className="flex items-center">
+							<span className="flex min-w-28 h-7 items-center gap-x-2">
+								<Clock size={16} />
+								Last edited
+							</span>
+							<span>29 Oct 2024</span>
+						</div>
+					</section>
+					<Separator className="my-4" />
+				</section>
 
 				{/* NOTE OPTIONS */}
 				<section className="basis-72 bg-white px-5 py-7 space-y-4">
