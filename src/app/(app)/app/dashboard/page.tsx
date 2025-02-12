57368a2f14e82ce07a50faa2401993c03a7d6f1d
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-12 19:32:12 +0100

    Put note list section code into its own component

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,79 +1,11 @@
 import NoteDetails from '@/components/note-details'
+import NoteList from '@/components/note-list'
 import NoteOptions from '@/components/note-options'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
 import { Textarea } from '@/components/ui/textarea'
 import { Archive, Clock, Settings, Tag, Trash } from 'lucide-react'
-
-const notes = [
-	{
-		id: 1,
-		title: 'React Performance Optimization',
-		tags: ['Dev', 'React'],
-		createdAt: '29 Oct 2024',
-	},
-	{
-		id: 2,
-		title: 'Japan Travel Planning',
-		tags: ['Travel', 'Personal'],
-		createdAt: '28 Oct 2024',
-	},
-	{
-		id: 3,
-		title: 'Favorite Pasta Recipes',
-		tags: ['Cooking', 'Recipes'],
-		createdAt: '27 Oct 2024',
-	},
-	{
-		id: 4,
-		title: 'Weekly Workout Plan',
-		tags: ['Dev', 'React'],
-		createdAt: '29 Oct 2024',
-	},
-	{
-		id: 5,
-		title: 'Meal Prep Ideas',
-		tags: ['Cooking', 'Health', 'Recipes'],
-		createdAt: '29 Oct 2024',
-	},
-	{
-		id: 3,
-		title: 'Favorite Pasta Recipes',
-		tags: ['Cooking', 'Recipes'],
-		createdAt: '27 Oct 2024',
-	},
-	{
-		id: 4,
-		title: 'Weekly Workout Plan',
-		tags: ['Dev', 'React'],
-		createdAt: '29 Oct 2024',
-	},
-	{
-		id: 5,
-		title: 'Meal Prep Ideas',
-		tags: ['Cooking', 'Health', 'Recipes'],
-		createdAt: '29 Oct 2024',
-	},
-	{
-		id: 3,
-		title: 'Favorite Pasta Recipes',
-		tags: ['Cooking', 'Recipes'],
-		createdAt: '27 Oct 2024',
-	},
-	{
-		id: 4,
-		title: 'Weekly Workout Plan',
-		tags: ['Dev', 'React'],
-		createdAt: '29 Oct 2024',
-	},
-	{
-		id: 5,
-		title: 'Meal Prep Ideas',
-		tags: ['Cooking', 'Health', 'Recipes'],
-		createdAt: '29 Oct 2024',
-	},
-]
 
 export default function DashboardPage() {
 	return (
@@ -90,36 +22,8 @@ export default function DashboardPage() {
 			</header>
 			<section className="flex flex-row flex-1 overflow-auto">
 				{/* NOTES LIST */}
-				<section className="basis-72  border-[#E0E4EA] border-r px-5  bg-white overflow-auto  ">
-					<div className="mb-4 sticky top-0 bg-white pt-7">
-						<Button className="w-full mb-4 ">Create new note</Button>
-					</div>
-					<section>
-						<ul className="space-y-2">
-							{notes.map((note) => (
-								<div key={Math.random()}>
-									<li className="p-2 hover:bg-[#F3F5F8] rounded-md hover:cursor-pointer !m-0">
-										<h3 className="font-semibold mb-3">{note.title}</h3>
-										<div className="space-x-2 mb-3">
-											{note.tags.map((tag) => (
-												<span
-													key={tag}
-													className="bg-[#E0E4EA] rounded-sm text-[12px] px-[6px] py-[2px]"
-												>
-													{tag}
-												</span>
-											))}
-										</div>
 
-										<p className="text-[12px]">{note.createdAt}</p>
-									</li>
-									<Separator className="!m-1 " />
-								</div>
-							))}
-						</ul>
-					</section>
-				</section>
-
+				<NoteList />
 				{/* NOTE DETAILS */}
 				<NoteDetails />
 				{/* NOTE OPTIONS */}
---
+++ src/components/note-list.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { Button } from './ui/button'
+import { Separator } from './ui/separator'
+
+const notes = [
+	{
+		id: 1,
+		title: 'React Performance Optimization',
+		tags: ['Dev', 'React'],
+		createdAt: '29 Oct 2024',
+	},
+	{
+		id: 2,
+		title: 'Japan Travel Planning',
+		tags: ['Travel', 'Personal'],
+		createdAt: '28 Oct 2024',
+	},
+	{
+		id: 3,
+		title: 'Favorite Pasta Recipes',
+		tags: ['Cooking', 'Recipes'],
+		createdAt: '27 Oct 2024',
+	},
+	{
+		id: 4,
+		title: 'Weekly Workout Plan',
+		tags: ['Dev', 'React'],
+		createdAt: '29 Oct 2024',
+	},
+	{
+		id: 5,
+		title: 'Meal Prep Ideas',
+		tags: ['Cooking', 'Health', 'Recipes'],
+		createdAt: '29 Oct 2024',
+	},
+	{
+		id: 3,
+		title: 'Favorite Pasta Recipes',
+		tags: ['Cooking', 'Recipes'],
+		createdAt: '27 Oct 2024',
+	},
+	{
+		id: 4,
+		title: 'Weekly Workout Plan',
+		tags: ['Dev', 'React'],
+		createdAt: '29 Oct 2024',
+	},
+	{
+		id: 5,
+		title: 'Meal Prep Ideas',
+		tags: ['Cooking', 'Health', 'Recipes'],
+		createdAt: '29 Oct 2024',
+	},
+	{
+		id: 3,
+		title: 'Favorite Pasta Recipes',
+		tags: ['Cooking', 'Recipes'],
+		createdAt: '27 Oct 2024',
+	},
+	{
+		id: 4,
+		title: 'Weekly Workout Plan',
+		tags: ['Dev', 'React'],
+		createdAt: '29 Oct 2024',
+	},
+	{
+		id: 5,
+		title: 'Meal Prep Ideas',
+		tags: ['Cooking', 'Health', 'Recipes'],
+		createdAt: '29 Oct 2024',
+	},
+]
+
+export default function NoteList() {
+	return (
+		<section className="basis-72  border-[#E0E4EA] border-r px-5  bg-white overflow-auto  ">
+			<div className="mb-4 sticky top-0 bg-white pt-7">
+				<Button className="w-full mb-4 ">Create new note</Button>
+			</div>
+			<section>
+				<ul className="space-y-2">
+					{notes.map((note) => (
+						<div key={Math.random()}>
+							<li className="p-2 hover:bg-[#F3F5F8] rounded-md hover:cursor-pointer !m-0">
+								<h3 className="font-semibold mb-3">{note.title}</h3>
+								<div className="space-x-2 mb-3">
+									{note.tags.map((tag) => (
+										<span
+											key={tag}
+											className="bg-[#E0E4EA] rounded-sm text-[12px] px-[6px] py-[2px]"
+										>
+											{tag}
+										</span>
+									))}
+								</div>
+
+								<p className="text-[12px]">{note.createdAt}</p>
+							</li>
+							<Separator className="!m-1 " />
+						</div>
+					))}
+				</ul>
+			</section>
+		</section>
+	)
+}
