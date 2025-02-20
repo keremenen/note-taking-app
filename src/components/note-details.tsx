3e9050997d20b37ecffa19b36fd94f52825392a6
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-20 18:31:58 +0100

    WIP change note details into form component

--- src/components/note-details.tsx
+++ src/components/note-details.tsx
@@ -6,9 +6,22 @@ import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
 import { useNoteContext } from '@/lib/hooks'
 import { Note } from '@prisma/client'
+import React, { useState } from 'react'
 
 export default function NoteDetails() {
 	const { selectedNote, addNoteMode } = useNoteContext()
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		const formData = new FormData(e.currentTarget)
+		const newNote = {
+			title: formData.get('title') as string,
+			tags: formData.get('tags') as string,
+			content: formData.get('content') as string,
+		}
+		console.log(newNote)
+	}
 
 	return (
 		<section className="flex-1 px-6 py-5 flex flex-col">
@@ -16,12 +29,14 @@ export default function NoteDetails() {
 				<EmptyNoteView />
 			) : (
 				<>
-					<NoteDetailsTitle title={selectedNote!.title} />
-					<NoteDetailsInfo note={selectedNote!} />
-					<Separator className="my-4" />
-					<Textarea className="flex-1" />
-					<Separator className="my-4" />
-					<NoteDetailsActions />
+					<form onSubmit={handleSubmit} className="h-full flex flex-col">
+						<NoteDetailsTitle title={selectedNote!.title} />
+						<NoteDetailsInfo note={selectedNote!} />
+						<Separator className="my-4" />
+						<Textarea className="flex-1" id="content" name="content" />
+						<Separator className="my-4" />
+						<NoteDetailsActions />
+					</form>
 				</>
 			)}
 		</section>
@@ -41,11 +56,14 @@ function EmptyNoteView() {
 }
 
 function NoteDetailsTitle({ title }: { title: string }) {
+	const [currentTitle, setCurrentTitle] = useState(title)
 	return (
 		<input
-			defaultValue={title}
+			id="title"
+			name="title"
+			value={currentTitle}
 			className="text-2xl font-bold w-full bg-inherit outline-none focus:ring-2 hover:ring-2 rounded-sm p-1 transition duration-300"
-			onChange={() => {}}
+			onChange={(e) => setCurrentTitle(e.target.value)}
 		/>
 	)
 }
@@ -60,6 +78,8 @@ function NoteDetailsInfo({ note }: Props) {
 				</span>
 
 				<input
+					id="tags"
+					name="tags"
 					onChange={() => {}}
 					value={note.tags}
 					className="w-full bg-inherit p-1 focus:ring-2 hover:ring-2 rounded-sm transition duration-300"
@@ -85,7 +105,7 @@ function NoteDetailsInfo({ note }: Props) {
 function NoteDetailsActions() {
 	return (
 		<div className="flex gap-4">
-			<Button>Save</Button>
+			<Button type="submit">Save</Button>
 			<Button variant="outline">Delete</Button>
 		</div>
 	)
