0d2d50056ab1a76261647fab9f9110cfb4931027
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-25 19:21:06 +0100

    Fix logo image browser warning

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,9 @@ import Image from 'next/image'
 export default function Logo({ className }: { className?: string }) {
 	return (
 		<div className={cn('flex items-center gap-2', className)}>
-			<Image src="/Vector.svg" alt="Note app logo" height={28} width={28} />
+			<div className="size-8 relative">
+				<Image src="/Vector.svg" alt="Note app logo" fill />
+			</div>
 			<p className="font-pacifico text-[1.4375rem]">Notes</p>
 		</div>
 	)
