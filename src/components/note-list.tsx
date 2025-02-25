4637858e9f0240153b3c15dc50178d1910e37d0a
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-25 18:00:36 +0100

    Add info when any of lists are empty

--- src/components/note-list.tsx
+++ src/components/note-list.tsx
@@ -29,47 +29,74 @@ export default function NotesList({ type }: { type?: 'archive' | 'active' }) {
 					Create new note
 				</Button>
 				{type === 'archive' && (
-					<p className="text-sm text-[#6B7280]">
+					<p className="text-sm text-[#6B7280] mb-4">
 						All your archived notes are stored here. You can restore or delete
 						them anytime.
 					</p>
 				)}
-			</div>
-			<section>
-				<ul className="space-y-2">
-					{currentNotes.map((note) => (
-						<li
-							className=" hover:bg-[#F3F5F8] rounded-md hover:cursor-pointer transition duration-300"
-							key={Math.random()}
-						>
-							<button
-								onClick={() => handleSetSelectedNoteId(note.id)}
-								className="text-left size-[100%] px-2 py-1"
-							>
-								<h3 className="font-semibold mb-3">{note.title}</h3>
-								<div className="space-x-2 mb-3">
-									{note.tags.split(',').map((tag) => (
-										<span
-											key={Math.random()}
-											className="bg-[#E0E4EA] rounded-sm text-[12px] px-[6px] py-[2px]"
-										>
-											{tag.trim()}
-										</span>
-									))}
-								</div>
 
-								<p className="text-[12px]">
-									{new Date(note.updatedAt).toLocaleDateString('en-GB', {
-										day: 'numeric',
-										month: 'short',
-										year: 'numeric',
-									})}
-								</p>
-							</button>
-						</li>
-					))}
-				</ul>
-			</section>
+				{currentNotes.length === 0 && type === 'archive' ? (
+					<EmptyListInfo>
+						<p>
+							No notes have been archived yet. Move notes here for safekeeping,
+							or
+							<span className="underline">{` `}create a new note</span>.
+						</p>
+					</EmptyListInfo>
+				) : currentNotes.length === 0 ? (
+					<EmptyListInfo>
+						<p>
+							You don’t have any notes yet. Start a new note to capture your
+							thoughts and ideas.
+						</p>
+					</EmptyListInfo>
+				) : null}
+			</div>
+			{currentNotes.length === 0 ? null : (
+				<section>
+					<ul className="space-y-2">
+						{currentNotes.map((note) => (
+							<li
+								className=" hover:bg-[#F3F5F8] rounded-md hover:cursor-pointer transition duration-300"
+								key={Math.random()}
+							>
+								<button
+									onClick={() => handleSetSelectedNoteId(note.id)}
+									className="text-left size-[100%] px-2 py-1"
+								>
+									<h3 className="font-semibold mb-3">{note.title}</h3>
+									<div className="space-x-2 mb-3">
+										{note.tags.split(',').map((tag) => (
+											<span
+												key={Math.random()}
+												className="bg-[#E0E4EA] rounded-sm text-[12px] px-[6px] py-[2px]"
+											>
+												{tag.trim()}
+											</span>
+										))}
+									</div>
+
+									<p className="text-[12px]">
+										{new Date(note.updatedAt).toLocaleDateString('en-GB', {
+											day: 'numeric',
+											month: 'short',
+											year: 'numeric',
+										})}
+									</p>
+								</button>
+							</li>
+						))}
+					</ul>
+				</section>
+			)}
+		</section>
+	)
+}
+
+function EmptyListInfo({ children }: { children: React.ReactNode }) {
+	return (
+		<section className="bg-[#F3F5F8] border-[#E0E4EA] border-2 rounded-md p-2 tracking-tight">
+			{children}
 		</section>
 	)
 }
