ba6324801a8f45e6e5243336b9632f3d74c53068
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-03-01 13:09:00 +0100

    Add settings options elements

--- src/components/settings-options.tsx
+++ src/components/settings-options.tsx
@@ -1,22 +1,36 @@
+import { DoorOpen, LockKeyhole, Sun, Type } from 'lucide-react'
+import { Button } from './ui/button'
+import { Separator } from './ui/separator'
+
 const settingsOptions = [
 	{
 		label: 'Color Theme',
-		icon: '',
+		icon: <Sun />,
 	},
 	{
 		label: 'Font Theme',
-		icon: '',
+		icon: <Type />,
 	},
 	{
 		label: 'Change Password',
-		icon: '',
-	},
-	{
-		label: 'Logout',
-		icon: '',
+		icon: <LockKeyhole />,
 	},
 ]
 
 export default function SettingsOptions() {
-	return <section>settings options</section>
+	return (
+		<section className="flex flex-col pl-8 pr-4 py-5 gap-y-2 w-full max-w-64 border-[#E0E4EA] border-r">
+			{settingsOptions.map((option, index) => (
+				<Button key={index} className="justify-start" variant={'outline'}>
+					{option.icon}
+					{option.label}
+				</Button>
+			))}
+			<Separator className="my-2" />
+			<Button className="justify-start" variant={'outline'}>
+				<DoorOpen />
+				Log Out
+			</Button>
+		</section>
+	)
 }
