c1f82186f17e31f4c63aa6ea45ede71402522b9d
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-03-06 21:03:09 +0100

    Refactor note-list component

--- src/components/note-list.tsx
+++ src/components/note-list.tsx
@@ -11,8 +11,7 @@ type NoteListProps = {
 
 export default function NotesList({ type }: NoteListProps) {
 	const { searchQuery } = useSearchContext()
-	const { notes, handleSetSelectedNoteId, handleActiveAddNoteMode } =
-		useNoteContext()
+	const { notes, handleActiveAddNoteMode } = useNoteContext()
 
 	const selectedTag = getSearchParams('tag')
 
@@ -48,7 +47,7 @@ export default function NotesList({ type }: NoteListProps) {
 
 	return (
 		<section className="basis-72  border-[#E0E4EA] border-r px-5  bg-white overflow-auto  ">
-			<div className="mb-4 sticky top-0 bg-white pt-7">
+			<section className="mb-4 sticky top-0 bg-white pt-7">
 				<Button className="w-full mb-4 " onClick={handleActiveAddNoteMode}>
 					Create new note
 				</Button>
@@ -64,76 +63,71 @@ export default function NotesList({ type }: NoteListProps) {
 					</NoteListAdditionalInfo>
 				) : null}
 
-				{filteredNotes.length === 0 && (
-					<EmptyListInfo>
-						{searchQuery ? (
-							<p>
-								No notes match your search. Try a different keyword or create a
-								new note.
-							</p>
-						) : type === 'active' ? (
-							<p>
-								You don’t have any notes yet. Start a new note to capture your
-								thoughts and ideas.
-							</p>
-						) : type === 'archive' ? (
-							<p>
-								No notes have been archived yet. Move notes here for
-								safekeeping, or{' '}
-								<span className="underline">create a new note</span>.
-							</p>
-						) : null}
-					</EmptyListInfo>
-				)}
-			</div>
+				{filteredNotes.length === 0 && <EmptyListInfo type={type} />}
+			</section>
 
-			{filteredNotes.length === 0 ? null : (
-				<section>
-					<ul className="space-y-2">
-						{filteredNotes.map((note) => (
-							<li
-								className=" hover:bg-[#F3F5F8] rounded-md hover:cursor-pointer transition duration-300"
-								key={Math.random()}
-							>
-								<button
-									onClick={() => handleSetSelectedNoteId(note.id)}
-									className="text-left size-[100%] px-2 py-1"
-								>
-									<h3 className="font-semibold mb-3">{note.title}</h3>
-									<div className="mb-3 flex gap-x-3 gap-y-2 flex-wrap">
-										{note.tags.split(',').map((tag) => (
-											<span
-												key={Math.random()}
-												className="bg-[#E0E4EA] rounded-sm text-[12px] px-[6px] py-[2px]"
-											>
-												{tag.trim()}
-											</span>
-										))}
-									</div>
-
-									<p className="text-[12px]">
-										{new Date(note.updatedAt).toLocaleDateString('en-GB', {
-											day: 'numeric',
-											month: 'short',
-											year: 'numeric',
-										})}
-									</p>
-								</button>
-							</li>
-						))}
-					</ul>
-				</section>
-			)}
+			<section>
+				<ul className="space-y-2">
+					{filteredNotes.map((note) => (
+						<SingleNoteSummary key={note.id} note={note} />
+					))}
+				</ul>
+			</section>
 		</section>
 	)
 }
 
-function EmptyListInfo({ children }: { children: React.ReactNode }) {
+function SingleNoteSummary({ note }: { note: Note }) {
+	const { handleSetSelectedNoteId } = useNoteContext()
+
 	return (
-		<section className="bg-[#F3F5F8] border-[#E0E4EA] border-2 rounded-md p-2 tracking-tight">
-			{children}
-		</section>
+		<li
+			className=" hover:bg-[#F3F5F8] rounded-md hover:cursor-pointer transition duration-300"
+			key={note.id}
+		>
+			<button
+				onClick={() => handleSetSelectedNoteId(note.id)}
+				className="text-left size-[100%] px-2 py-1"
+			>
+				<h3 className="font-semibold mb-3">{note.title}</h3>
+				<div className="mb-3 flex gap-x-3 gap-y-2 flex-wrap">
+					{note.tags.split(',').map((tag) => (
+						<span
+							key={Math.random()}
+							className="bg-[#E0E4EA] rounded-sm text-[12px] px-[6px] py-[2px]"
+						>
+							{tag.trim()}
+						</span>
+					))}
+				</div>
+
+				<p className="text-[12px]">
+					{new Date(note.updatedAt).toLocaleDateString('en-GB', {
+						day: 'numeric',
+						month: 'short',
+						year: 'numeric',
+					})}
+				</p>
+			</button>
+		</li>
 	)
+}
+
+function EmptyListInfo({ type }: { type?: 'archive' | 'active' }) {
+	const { searchQuery } = useSearchContext()
+	const message = searchQuery
+		? 'No notes match your search. Try a different keyword or create a new note.'
+		: type === 'active'
+		? 'You don’t have any notes yet. Start a new note to capture your thoughts and ideas.'
+		: type === 'archive'
+		? 'No notes have been archived yet. Move notes here for safekeeping, or create a new note.'
+		: null
+
+	return message ? (
+		<section className="bg-[#F3F5F8] border-[#E0E4EA] border-2 rounded-md px-2 py-1 mt-4 text-sm tracking-tight">
+			<p>{message}</p>
+		</section>
+	) : null
 }
 
 function NoteListAdditionalInfo({ children }: { children: string | string[] }) {
