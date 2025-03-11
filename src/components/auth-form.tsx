9a9f5bc0ec762d6c53f781ccef1719da5d583486
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-03-11 18:15:18 +0100

    Add type prop to auth-form

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -2,7 +2,11 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Button } from './ui/button'
 
-export default function AuthForm() {
+export default function AuthForm({
+	type,
+}: {
+	type: 'login' | 'signup' | 'reset'
+}) {
 	return (
 		<form action="" className="w-full ">
 			<div>
