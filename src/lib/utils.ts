2f0dfd71d0d70659e254513f17fff501f1b38dfb
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-03-12 19:43:23 +0100

    Remove unused import

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
 import { Note } from '@prisma/client'
 import { clsx, type ClassValue } from 'clsx'
-
 import { twMerge } from 'tailwind-merge'
-import { NoteEssetials } from './types'
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
